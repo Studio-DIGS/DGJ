38ed9dd0854e09a482d91343994fba101258a023
jucobee <52049294+jucobee@users.noreply.github.com>
2022-05-22 22:49:32 -0700

    submitted version

--- Assets/Scripts/WorldBounds.cs
+++ Assets/Scripts/WorldBounds.cs
@@ -14,7 +14,7 @@ public class WorldBounds : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (pathCreator != null)
         {
